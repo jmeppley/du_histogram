9c0904c4ed2b607739f3a1478ca2ea6763d6a55e
John Eppley <jmeppley@gmail.com>
2017-07-31 10:25:44 -1000

    Creating install script and README

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # du_histogram
 Simple ASCII histogram of du (disk usage) output
+
+## What it does
+This script runs the "du" utility and prints a text histogram of the output sorted by size
+or date.
+
+## Installation
+### Requirements
+It should work with python 2 or 3 on any unix-like system with "du" installed. It
+needs just two non-standard python packages:
+
+ * docopt
+ * numpy
+
+### Installation
+You can call the script directly from the repo:
+
+```
+./duhist.py
+```
+
+Or install it with setuptools:
+
+```
+python setup.py install
+```
+
+## Usage
+### Basic
+Calling the script with no arguments will git a histograms of files and folders
+in the current path sorted by size.
+
+### Advanced
+```
+Usage:
+    duhist.py [-ltX] [-w WIDTH] [<directory>]
+    duhist.py -h | --help
+    duhist.py --version
+
+Options:
+    -h, --help     Show this screen.
+    --version      Show version.
+    -l, --log      Use log scale. (Chars - ~ = and # indicate order of mag)
+    -t, --time     Print and sort by age. (10m -> 10 months, 5h -> 5 hours)
+    -X, --allfs    Cross file system boundaries (don't use du -x)
+    -w <WIDTH>     Width of text to print [default: 80]
+```
---
+++ setup.py
@@ -0,0 +1,38 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+DESCRIPTION = "Simply ASCII histogram of du output"
+LONG_DESCRIPTION = open('README.md').read()
+NAME = "du_histogram"
+AUTHOR = "John Eppley"
+AUTHOR_EMAIL = "jmeppley@gmail.com"
+MAINTAINER = "John Eppley"
+MAINTAINER_EMAIL = "jmeppley@gmail.com"
+URL = 'http://github.com/jmeppley/du_histogram'
+DOWNLOAD_URL = 'http://github.com/jmeppley/du_histogram'
+LICENSE = 'Apache'
+VERSION = '2.0'
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      url=URL,
+      download_url=DOWNLOAD_URL,
+      license=LICENSE,
+      packages=[,],
+      scripts=['duhist.py',],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GPL License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2.7'],
+      )
