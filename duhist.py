50c02668007b42b2ac1d3797c357be4f0f117559
John Eppley <jmeppley@gmail.com>
2017-08-08 12:43:37 -1000

    changing switch to be more universal

--- duhist.py
+++ duhist.py
@@ -137,9 +137,9 @@ def main(arguments):
 
     # run du
     one_fs_arg = "-x" if one_fs else ""
-    print('command: du -d 0 %s -k *' % (one_fs_arg))
+    print('command: du --max-depth 0 %s -k *' % (one_fs_arg))
     print('cwd: %s' % (directory))
-    p=subprocess.Popen('du -d 0 %s -k *' % (one_fs_arg),
+    p=subprocess.Popen('du --max-depth 0 %s -k *' % (one_fs_arg),
                        shell=True,
                        stdout=subprocess.PIPE,
                        cwd=directory)
