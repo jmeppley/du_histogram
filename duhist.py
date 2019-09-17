a7b9ddedfd236c74231eda8af5819eac03511a5f
Marmaduke <jmeppley@gmail.com>
2019-09-17 16:48:24 -0400

    added support for multiple paths

--- duhist.py
+++ duhist.py
@@ -2,11 +2,11 @@
 """
 Disk Usage Histogram
 
-Calculates the usage of all files in the given (or current) directory and plots
-an ASCII histogram of sizes. Omit directory name to process current dir.
+Calculates the usage of all files in the given (or current) path(s) and plots
+an ASCII histogram of sizes. Omit path name to process current dir.
 
 Usage:
-  duhist.py [options] [<directory>]
+  duhist.py [options] [<path>...]
   duhist.py -h | --help
   duhist.py --version
 
@@ -35,41 +35,41 @@ def get13charName(name, name_width=13):
         return name[:start_end]+'***'+name[end_start:]
     while len(name) < name_width:
         # pad with trailing space to get to 13 characters
-        name+=' '
+        name += ' '
     return name
 
 def getBar(value, scale, log):
     """
     return string of equal signs ('=') based on value and scale
     """
-    width=value/scale
+    width = value/scale
     if width<1:
         return ''
-    char='='
-    s=char
+    char = '='
+    s = char
     while len(s)<width:
         if log:
-            char=logChars[int(math.ceil(len(logChars)*value/float(maxVal))-1)]
-        s+=char
+            char = logChars[int(math.ceil(len(logChars)*value/float(maxVal))-1)]
+        s += char
     return s
 
-logChars=['-','~','=','#']
+logChars = ['-', '~', '=', '#']
 def getLogBar(value, scale, maxWidth, log):
     """
     return string of various signs (-,~,=,#) based on value and scale
     """
-    width=value/scale
-    if width<1:
+    width = value / scale
+    if width < 1:
         return ''
-    char=logChars[0]
-    s=char
-    while len(s)<width:
+    char = logChars[0]
+    s = char
+    while len(s) < width:
         if log:
-            char=logChars[int(math.ceil(len(logChars)*len(s)/float(maxWidth))-1)]
-        s+=char
+            char = logChars[int(math.ceil(len(logChars)*len(s)/float(maxWidth))-1)]
+        s += char
     return s
 
-steps=['K','M','G','T','P']
+steps = ['K','M','G','T','P']
 def getSizeString(val):
     """
     Given number of kilobytes, return string like 34K or .4M or 4T.
@@ -77,21 +77,21 @@ def getSizeString(val):
     """
 
     # first, figure out ourder of magnitude
-    size=float(val)
-    step=0
-    while size>99:
-        size/=1024
-        step+=1
+    size = float(val)
+    step = 0
+    while size > 99:
+        size /= 1024
+        step += 1
 
     # next, format
-    if size>=.95:
-        size='%2.f' % size
+    if size >= .95:
+        size = '%2.f' % size
     else:
-        size='%.1f' % size
-        size=size[-2:]
+        size = '%.1f' % size
+        size = size[-2:]
 
     # add suffix
-    size+=steps[step]
+    size += steps[step]
     return size
 
 units = {'h':3600, 'd':24*3600, 'm':24*3600*30, 'y':24*3600*365}
@@ -104,11 +104,43 @@ def get3charAge(age_s):
     else:
         return "00h"
 
-def getLastModDate(path, directory):
+def du_directory(directory, one_fs):
     """
-    Return the last modification date of this thing
+    get folder contents by cd-ing in and using glob (*)
     """
-    return os.path.getmtime(os.path.sep.join([directory,path]))
+    # run du
+    one_fs_arg = "-x" if one_fs else ""
+    print('command: du --max-depth 0 %s -k *' % (one_fs_arg))
+    print('cwd: %s' % (directory))
+    p = subprocess.Popen('du --max-depth 0 %s -k *' % (one_fs_arg),
+                       shell=True,
+                       stdout=subprocess.PIPE,
+                       cwd=directory)
+
+    # get file and size from each line
+    for line in p.stdout:
+        if not isinstance(line, str):
+            line = line.decode()
+        cells = line.rstrip('\n\r').split(None,1)
+        yield cells[1], int(cells[0])
+
+def du_file(filename, one_fs):
+    """
+    get file size with du
+    """
+    # run du
+    one_fs_arg = "-x" if one_fs else ""
+    print('command: du --max-depth 0 %s -k %s' % (one_fs_arg, filename))
+    p = subprocess.Popen('du --max-depth 0 %s -k %s' % (one_fs_arg, filename),
+                       shell=True,
+                       stdout=subprocess.PIPE)
+
+    # get file and size from each line
+    line = next(iter(p.stdout))
+    if not isinstance(line, str):
+        line = line.decode()
+    cells = line.rstrip('\n\r').split(None,1)
+    return int(cells[0])
 
 ###########
 # Main code
@@ -121,81 +153,79 @@ def main(arguments):
           "--log": true, 
           "--time": true, 
           "--version": false, 
-          "<directory>": "."
+          "<path>": "."
     }
     """
-    ##TODO: fix log and timeSort arguments
+    ##TODO: fix log and time_sort arguments
     try:
         log = arguments["--log"]
-        timeSort = arguments["--time"]
+        time_sort = arguments["--time"]
         one_fs = not arguments["--allfs"]
         max_width = int(arguments["-w"])
         name_width = int(arguments["-W"])
-        directory=arguments['<directory>']
+        du_paths = arguments['<path>']
     except KeyError:
         print(repr(arguments))
         raise
-    if directory is None:
-        directory = "."
+    if du_paths is None or len(du_paths) == 0:
+        du_paths = [".",]
 
     if name_width < 5:
         raise Exception("The name column must be at least 5 characters wide!")
 
-    # run du
-    one_fs_arg = "-x" if one_fs else ""
-    print('command: du --max-depth 0 %s -k *' % (one_fs_arg))
-    print('cwd: %s' % (directory))
-    p=subprocess.Popen('du --max-depth 0 %s -k *' % (one_fs_arg),
-                       shell=True,
-                       stdout=subprocess.PIPE,
-                       cwd=directory)
-    sizeMap={}
+    size_map = {}
+    if time_sort:
+        date_map = {}
 
-    # get file and size from each line
-    for line in p.stdout:
-        if not isinstance(line, str):
-            line = line.decode()
-        cells=line.rstrip('\n\r').split(None,1)
-        sizeMap[cells[1]]=int(cells[0])
+    for path in du_paths:
+        if os.path.isdir(path):
+            for name, size in du_directory(path, one_fs):
+                full_name = os.path.join(path, name)
+                # hide dir name if only processing one
+                map_name = name if len(du_paths) == 1 else full_name
+                size_map[map_name] = size
+                if time_sort:
+                    date_map[map_name] = os.path.getmtime(full_name)
+        else:
+            size_map[path] = du_file(path, one_fs)
+            if time_sort:
+                date_map[path] = os.path.getmtime(path)
 
     # sort names by size
-    if timeSort:
-        date_map={}
+    if time_sort:
         now = time.time()
-        for x in sizeMap:
-            date_map[x]=getLastModDate(x,directory)
-        sorted_names=sorted(sizeMap.keys(),key=lambda x: date_map[x])
+        sorted_names = sorted(size_map.keys(),key=lambda x: date_map[x])
     else:
-        sorted_names=sorted(sizeMap.keys(),key=lambda x: sizeMap[x])
+        sorted_names = sorted(size_map.keys(),key=lambda x: size_map[x])
 
     # empty directory
-    if len(sizeMap)==0:
+    if len(size_map) == 0:
         sys.exit("ERROR: %s is empty" % (directory))
 
     # figure out range (may be more efficient to do while parsing, but not worth it)
     label_width = name_width + 6
-    if timeSort:
+    if time_sort:
         label_width += 4
     width = max_width - label_width
-    tot=sum(sizeMap.values())
-    maxVal=max(sizeMap.values())
+    tot = sum(size_map.values())
+    maxVal = max(size_map.values())
 
     # log
     if log:
-        maxVal=math.log(maxVal)
+        maxVal = math.log(maxVal)
 
-    scale=maxVal/float(width)
+    scale = maxVal/float(width)
 
     # print histogram
     for name in sorted_names:
-        size=sizeMap[name]
+        size = size_map[name]
         if log and size>0:
-            barSize=math.log(size)
-            barString=getLogBar(barSize,scale,width, log)
+            barSize = math.log(size)
+            barString = getLogBar(barSize,scale,width, log)
         else:
-            barSize=size
-            barString=getBar(barSize,scale, log)
-        if timeSort:
+            barSize = size
+            barString = getBar(barSize,scale, log)
+        if time_sort:
             sys.stdout.write(get3charAge(now - date_map[name]))
             sys.stdout.write(" ")
         if os.path.isdir(name):
@@ -207,5 +237,5 @@ def main(arguments):
     sys.stdout.write("Total: %s\n" % (getSizeString(tot)))
 
 if __name__ == '__main__':
-    arguments=docopt(__doc__, version='duhist 0.9.1')
+    arguments = docopt(__doc__, version='duhist 0.9.1')
     main(arguments)
